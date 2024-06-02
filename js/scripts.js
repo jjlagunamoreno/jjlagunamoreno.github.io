ae89aee456a9d6ced5b6332e4df4207d3b345be7
jj <zx21student017@colegiovalledelmiro.es>
2024-06-02 19:30:09 +0200

    javascript bien con menu en carta

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     const categories = document.querySelectorAll('.category');
 
     categories.forEach(category => {
-        category.addEventListener('click', function () {
-            const moreProducts = category.querySelector('.more-products');
+        category.addEventListener('click', function() {
+            const categoryData = category.querySelector('h3').getAttribute('data-category');
+            const moreProducts = document.getElementById(categoryData);
             const icon = category.querySelector('i');
 
             if (moreProducts.classList.contains('d-none')) {
@@ -28,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
         icon.classList.add('fa-sun');
     }
 
-    toggleButton.addEventListener('click', function () {
+    toggleButton.addEventListener('click', function() {
         document.body.classList.toggle('dark-mode');
         if (document.body.classList.contains('dark-mode')) {
             icon.classList.remove('fa-moon');
@@ -41,3 +42,18 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+function toggleCategory(categoryId) {
+    const moreProducts = document.getElementById(categoryId);
+    const icon = document.querySelector(`[data-category="${categoryId}"] i`);
+
+    if (moreProducts.classList.contains('d-none')) {
+        moreProducts.classList.remove('d-none');
+        icon.classList.remove('fa-angle-down');
+        icon.classList.add('fa-angle-up');
+    } else {
+        moreProducts.classList.add('d-none');
+        icon.classList.remove('fa-angle-up');
+        icon.classList.add('fa-angle-down');
+    }
+}
